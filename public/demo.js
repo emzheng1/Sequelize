d785c0021f3a76da40e3d556bccdba5b85b35d66
Emily Zheng <69724738+emzheng1@users.noreply.github.com>
2021-04-12 00:33:33 -0400

    Random number and display data

---
+++ public/demo.js
@@ -0,0 +1,27 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+async function getMeals() {
+  console.log('data request');
+  const diningRequest = await fetch('/api/wholeMeal');
+  const diningData = await diningRequest.json();
+  return diningData;
+}
+
+async function windowActions() {
+  console.log('loaded window');
+  const results = await getMeals();
+  const meals = results.data;
+
+  const mealArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const selectedMeals = mealArray.map((element) => {
+    const random = getRandomIntInclusive(0, meals.length - 1);
+    return meals[random];
+  });
+  console.table(selectedMeals);
+}
+
+window.onload = windowActions;
